13523358622ae0949ebf6f6a289ce33676829072
933-ship-it <anybody12351@gmail.com>
2025-06-20 21:33:37 +0300

    Update serve-app.js

--- api/serve-app.js
+++ api/serve-app.js
@@ -1,16 +1,17 @@
 // File: api/serve-app.js
-import { readFile } from 'fs/promises';
-import path from 'path';
+const fs = require('fs');
+const path = require('path');
 
-export default async function handler(req, res) {
+module.exports = (req, res) => {
   const filePath = path.join(process.cwd(), 'private', 'production-app.js');
 
-  try {
-    const fileContents = await readFile(filePath, 'utf8');
-    res.setHeader('Content-Type', 'application/javascript');
-    res.status(200).send(fileContents);
-  } catch (error) {
-    console.error('File read error:', error);
-    res.status(500).send('// Error loading script');
-  }
-}
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading file:', err);
+      res.status(500).send('// Failed to load script');
+    } else {
+      res.setHeader('Content-Type', 'application/javascript');
+      res.status(200).send(data);
+    }
+  });
+};
